9c13634baa2a5c230aabfd220693b865b31759b0
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-12-30 13:00:22 +0630

    extract icon components

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
+import IconClosed from '../components/IconClosed';
+import IconOpened from '../components/IconOpened';
+import CommentsIcon from '../components/CommentsIcon';
 export default function Issues() {
   async function fetchIssues() {
     return fetch(
@@ -17,11 +20,8 @@ export default function Issues() {
   const {
     data: issues,
     isSuccess,
-    isError,
     isLoading,
-    error,
   } = useQuery('issues', fetchIssues);
-  console.log(isError, isSuccess);
   return (
     <>
       {isLoading && <span>loading ...</span>}
@@ -31,37 +31,11 @@ export default function Issues() {
             <a href="#">facebook / create-react-app</a>
             <div className="open-closed-buttons">
               <button>
-                <svg
-                  className="open"
-                  viewBox="0 0 16 16"
-                  version="1.1"
-                  width="16"
-                  height="16"
-                  aria-hidden="true"
-                >
-                  <path d="M8 9.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z"
-                  ></path>
-                </svg>
+                <IconOpened />
                 <span className="font-bold">96 Open</span>
               </button>
               <button>
-                <svg
-                  className="closed"
-                  viewBox="0 0 16 16"
-                  version="1.1"
-                  width="16"
-                  height="16"
-                  aria-hidden="true"
-                >
-                  <path d="M11.28 6.78a.75.75 0 00-1.06-1.06L7.25 8.69 5.78 7.22a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l3.5-3.5z"></path>
-                  <path
-                    fill-rule="evenodd"
-                    d="M16 8A8 8 0 110 8a8 8 0 0116 0zm-1.5 0a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z"
-                  ></path>
-                </svg>
+                <IconClosed />
                 <span className="">254 Closed</span>
               </button>
             </div>
@@ -70,20 +44,7 @@ export default function Issues() {
             {issues.map(issue => (
               <div key={issue.id} className="issues-entry">
                 <div className="issues-entry-title-container">
-                  <svg
-                    className="open"
-                    viewBox="0 0 16 16"
-                    version="1.1"
-                    width="16"
-                    height="16"
-                    aria-hidden="true"
-                  >
-                    <path d="M8 9.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 0a8 8 0 100 16A8 8 0 008 0zM1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0z"
-                    ></path>
-                  </svg>
+                  <IconOpened />
                   <div className="issues-title">
                     <Link to={`/details/${issue.id}`}>{issue.title}</Link>
                     <div className="issues-title-details">
@@ -92,19 +53,7 @@ export default function Issues() {
                   </div>
                 </div>
                 <a href="#" className="comments-count-container">
-                  <svg
-                    className="octicon octicon-comment v-align-middle"
-                    viewBox="0 0 16 16"
-                    version="1.1"
-                    width="16"
-                    height="16"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M2.75 2.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h2a.75.75 0 01.75.75v2.19l2.72-2.72a.75.75 0 01.53-.22h4.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25H2.75zM1 2.75C1 1.784 1.784 1 2.75 1h10.5c.966 0 1.75.784 1.75 1.75v7.5A1.75 1.75 0 0113.25 12H9.06l-2.573 2.573A1.457 1.457 0 014 13.543V12H2.75A1.75 1.75 0 011 10.25v-7.5z"
-                    ></path>
-                  </svg>
+                  <CommentsIcon />
                   <div class="comments-count">{issue.comments}</div>
                 </a>
               </div>
@@ -112,7 +61,6 @@ export default function Issues() {
           </div>
         </div>
       )}
-      {isError && <div>{error.message}</div>}
     </>
   );
 }
